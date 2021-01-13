cc4f2398fbc75ace5dea1e1baa96fadf5c733669
goolace <eushuen16@gmail.com>
2021-01-13 20:05:04 +1100

    Refactored code

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # API-for-Next-to-Jump
-API to return a list of "Next to Jump" races. 
+API to return a list of "Next-to-Jump" races. 
+
+#### Definition of "Next-to-Jump":
+The remaining races as of today. <br />
+Races that are in the past are not returned. <br />
 
 #### Input:
-CSV file
+CSV file with the specified headers as below. <br />
 
 #### Output:
-Race type, race number, race name, venue and the race start time, ordered by race time in ascending order.
+Race type, race info, race venue and the race start time, sorted ascending in the importance as below: <br />
+1. Race start time
+2. Race number
+3. Race name
 
 ### Assumptions:
-API takes in CSV files with headers of order: <br />
+API takes in CSV files with HEADERS of order: <br />
 1. race_type
 2. race_number
 3. race_name
@@ -18,9 +25,14 @@ API takes in CSV files with headers of order: <br />
 #### Mandatory attributes & types:
 race_type       : categorical <br />
 race_venue      : string <br />
-race_start_time : string, (format yyyy-MM-dd HH:mm:ss) <br />
+race_start_time : datetime object, (format yyyy-MM-dd HH:mm:ss) <br />
 
 #### Optional attributes & types:
 (At least one must be present) <br />
 race_number     : integer <br />
 race_name       : string <br />
+
+#### Removes:
+- Duplicated rows (retains the first occurrence)
+- Rows containing empty entries of race_type, race_venue, race_startime
+- Rows containing empty entries of BOTH race_number and race_name
--- api_utils.py
+++ api_utils.py
@@ -4,22 +4,58 @@ import datetime as dt
 
 # TODO: pretty print
 def pretty_print_tabular():
-
- 
+    raise NotImplementedError
     return
 
-def clean_data(filename):
+def _select_valid_race(df):
     '''
-        Input   - filename
-        Output  - race_type, race_info (race_number and race_name concatenated), race_venue, race_time
+        Input   - pandas dataframe
+        Output  - The index of rows containing neither race number nor race name
+    '''
+    row_index, _ = np.where(pd.isnull(df))
 
-        race_info attribute:
+    # assert col_index is in range 1-2
+    curr = -1
+    index = []
+    for item in row_index:
+        if item == curr:
+            # print("duplicate")
+            index.append(item)
+        curr = item
+    
+    return index
+
+def _concat_race_info(df):
+    '''
+    Function concatenates race_venue and race_time into race_info for front-end display.
+        Input   -  pandas dataframe of next-to-jump races
+        Output  -  a list containing the race info concatenated as below from race_venue and race_time:
+
             if race_number is not present, print:
                 <race_name>
             if race_name is not present, print:
                 Race <race_number>
             if both are present, print:
                 Race <race_number> - <race_name>
+    '''
+
+    race_info = []
+    for num, name in zip(df["race_number"], df["race_name"]):
+        if pd.isna(name):
+            race_info.append(f"Race {num}")
+            continue
+
+        if pd.isna(num):
+            race_info.append(f"{name}")
+        else:
+            race_info.append(f"Race {num} - {name}")
+    
+    return race_info
+
+def clean_data(filename):
+    '''
+        Input   - filename
+        Output  - race_type, race_info (race_number and race_name concatenated), race_venue, race_time
     '''
 
     # Check that CSV is in a required format and enforce the necessary types
@@ -29,37 +65,16 @@ def clean_data(filename):
         parse_dates=[4], date_parser=pd.to_datetime)
 
     # handle duplicated rows
-    # TODO: choose to select one of the row instead
     df = df.drop_duplicates()
 
     # check mandatory fields
     df.dropna(subset=["race_type", "race_venue", "race_start_time"], inplace=True)
 
-    '''Attributes that are optional:
-        - Race number
-        - Race name
-        - Must have either one of them
-    '''
-
-    row_index, col_index = np.where(pd.isnull(df))
-
-    # assert col_index is in range 1-2
-    def _get_duplicated_index(row_index):
-        curr = -1
-        index = []
-        for item in row_index:
-            if item == curr:
-                # print("duplicate")
-                index.append(item)
-            curr = item
-        
-        return index
-
-    duplicated_index = _get_duplicated_index(row_index)
+    # remove invalid rows containing neither race number nor race name
+    duplicated_index = _select_valid_race(df)
     df.drop(df.index[duplicated_index], inplace=True)
 
-    # Assume: only require the today's race with the time
-    # Select Next-to-jump races for today only
+    # select Next-to-jump races for today 
     date_today = dt.date.today()
 
     selected_data = df[(df.race_start_time.dt.date == date_today)
@@ -72,18 +87,7 @@ def clean_data(filename):
     selected_data["race_start_time"] = selected_data.race_start_time.dt.strftime("%H.%M")
 
     # construct the race info based on race_number and race_name
-    race_info = []
-    for num, name in zip(selected_data["race_number"], selected_data["race_name"]):
-        if pd.isna(name):
-            race_info.append(f"Race {num}")
-            continue
-
-        if pd.isna(num):
-            race_info.append(f"{name}")
-        else:
-            race_info.append(f"Race {num} - {name}")
-
-    selected_data["race_info"] = race_info
+    selected_data["race_info"] = _concat_race_info(selected_data)
 
     return selected_data[["race_type", "race_venue", "race_start_time", "race_info"]]
 
